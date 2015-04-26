1ef58d486a447cdd373c747051a7819b6dbccd67
Sam Lozano <sam.lozano@gmail.com>
2015-04-25 19:49:12 -0500

    x

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Sam Lozano on 4/25/15.
+//  Copyright (c) 2015 Sam Lozano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Sam Lozano on 4/25/15.
+//  Copyright (c) 2015 Sam Lozano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
